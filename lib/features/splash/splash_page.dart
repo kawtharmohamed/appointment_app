17bc2b94cf0e053294ced136e9175b6faec146bc
kawtharmohamed <96089519+kawtharmohamed@users.noreply.github.com>
2023-12-28 06:00:42 -0800

    splash done

---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:appointment_app/features/onboarding/presentation/pages/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/splash.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      nextScreen: const OnBoardingPage(),
+    );
+  }
+}
